71febda493cdc5a93e8c2270590b4db8f01bb937
Chizz98 <Hamsteren_123>
2025-11-12 12:21:58 +0100

    added base script for merging fam files:

---
+++ scripts/merge_fam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Author: Chris Dijkstra
+Date: 12-11-2025
+
+Takes a .fam file and a phenotype file, merges phenotype into fam file. 
+"""
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
